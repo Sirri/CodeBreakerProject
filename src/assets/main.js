cceec51d3d1900deaf2f18191726edacab0d6451
Sirri <aki.koikkalainen@gmail.com>
2017-06-25 16:09:49 +0300

    some more stuff

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,8 +4,24 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+
+    if(answer == '' || attempt == ''){
+      setHiddenFields
+    }
 }
 
 function setHiddenFields (){
-  answer = Math.floor(Math.random()*10);
+  //Set answer to integer between 0-9999
+  answer = Math.floor(Math.random()*10000);
+
+  //set answer to string and add zeroes if needed
+  answer = answer.toString()
+
+  while (answer.length < 4){
+    answer = "0" + answer;
+  }
+  //set attemnts to 0
+  attempt = 0;
+
+
 }
